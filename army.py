a74b2d0f7b00c348f5bb87dcba6ba8ed1f26b52c
AlyaFL <82833193+AlyaFL@users.noreply.github.com>
2021-07-05 22:16:11 +0300

    Add files via upload

---
+++ army.py
@@ -0,0 +1,70 @@
+from squads import Squad
+from statistics import geometric_mean
+import settings
+
+
+class Army:
+    """Army class 
+
+    Contains a group of units (squad)
+
+    Attributes:
+        name: army name
+        strategy: strategy name
+        squad: list of squad
+    """
+    def __init__(self, name, strategy, random=None):
+        self.name = name
+        self.strategy = strategy
+        self.squads = []
+        self.random = random
+        self.create_squads()
+        
+
+    @property
+    def health(self):
+        """Army health value
+        
+        Returns:
+            health(int): sum of health of all squad in army
+        """
+        return sum(sq.health for sq in self.squads)
+
+    @property
+    def damage(self):
+        """Army damage value
+        
+        Returns:
+            (int): sum of damage value of all squad in army
+        """ 
+        return sum(sq.damage for sq in self.squads)
+
+    @property
+    def attack_success_probability(self):
+        """Army attack success probability
+        
+        Returns:
+            (int): average of the attack success probability of army squad
+        """
+        return geometric_mean(sq.attack_success_probability for sq in self.squads)
+
+    def attack(self, enemy):
+        enemy.get_damage(self.damage)
+        for sq in self.squads:
+            squad.get_experience()
+
+    def get_damage(self, value):
+        """Amount of damage army can get from enemy
+        
+        Args:
+            value: int
+        """
+        for sq in self.squads:
+            sq.get_damage(value)
+            if sq.health <= 0:
+                self.squad.remove(sq)
+
+    def create_squads(self):
+        """Fills squads[] with Squad instance"""
+        for _ in range(settings.COUNT_OF_SQUADS_PER_ARMY):
+            self.squads.append(Squad(self.strategy, self.random))
---
+++ vehicles.py
@@ -0,0 +1,86 @@
+from units import Unit
+from soldiers import Soldier
+from statistics import geometric_mean
+from random import choice
+
+
+class Vehicle(Unit):
+    """ Vehicle class 
+
+    Attributes:
+        name: unit type(vehicle)
+        health: represents the health of the unit 
+        recharge: represents the number of ms required to recharge the unit for an attack
+        operators: the number of soldiers required to operate the vehicle
+
+    """
+
+    def __init__(self, random=None):
+        self.random = random
+        self._health = 100
+        self.operators = []
+        self.create_operators()
+
+    @property
+    def damage(self):  
+        """The amount of damage a vehicle can afflict
+        
+        Returns:
+            int
+        """  
+        return 0.1 + sum(operators.experience for operators in self.operators) / 100
+
+    @property
+    def attack_success_probability(self):
+        """Vehicles attack success probability
+        
+        Returns:
+            int
+        """
+        return 0.5 * (1 + self.health / 
+        100) * geometric_mean(operator.attack_success_probability 
+                for operator in self.operators)
+
+    #def is_alive(self):
+     #   return (self.health > 0 and
+      #  bool(sum(op.is_alive for op in self.operators)))
+
+    @property
+    def health(self):
+        """Vehicles health value
+        
+        Returns:
+            health: int
+        """
+        return (sum(op.health for op in self.operators) /
+        len(self.operators) + self._health)
+
+    @health.setter
+    def health(self, value):
+        self._health = value
+
+    def get_damage(self, value):
+        """Amount of damage vehicles can get from enemy
+        
+        Args:
+            value: int
+        """
+        vehicle_damage = value * 0.6
+        operator_damage = value * 0.2
+        other_damage = value * 0.1
+        self.health -= vehicle_damage
+        active_operators = [op for op in self.operators]
+        random_operators = choice(active_operators)
+        other_operators = [op for op in active_operators if op is not random_operators]
+        for operator in self.operators:
+            operator.get_damage(operator_damage)
+            if operator.health < 0:
+                self.operators.remove(operator)
+
+    def get_experience(self):
+        self.experience += 1
+
+    def create_operators(self):
+        """Fills operators[] with Soldier instance"""
+        for _ in range(self.random.randint(1, 3)):
+            self.operators.append(Soldier(self.random))
